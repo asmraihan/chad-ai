f3d76602c244cc7bb33bbabaafb617e7bf8eee85
asmraihan <asmraihanctg14@gmail.com>
2023-07-25 18:31:16 +0600

    navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import AppNavigation from './app/navigation';
+
 
 export default function App() {
   return (
-    <View className="flex w-full h-full justify-center items-center bg-slate-900">
-      <Text className='text-lg text-white font-semibold'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <AppNavigation />
   );
 }
---
+++ app/navigation/index.tsx
@@ -0,0 +1,24 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home'
+import Welcome from '../screens/Welcome'
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
